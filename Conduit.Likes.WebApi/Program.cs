5d4fca6a056671fdb25778800ca289831e86cf1a
Alex2021cyberhuman <alex2021cyberhuman@gmail.com>
2022-02-28 20:46:13 +0500

    feat: Update shared-core

--- Conduit.Likes.WebApi/Program.cs
+++ Conduit.Likes.WebApi/Program.cs
@@ -14,6 +14,7 @@ using Conduit.Shared.Events.Models.Likes.Unfavorite;
 using Conduit.Shared.Events.Services.RabbitMQ;
 using Conduit.Shared.Startup;
 using Conduit.Shared.Tokens;
+using Conduit.Shared.Validation;
 using Microsoft.IdentityModel.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,7 +25,8 @@ var services = builder.Services;
 var environment = builder.Environment;
 var configuration = builder.Configuration;
 
-services.AddControllers();
+services.AddControllers()
+    .RegisterValidateModelAttribute();
 services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1",
